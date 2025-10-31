5db52b02c533383e2d3736f89f3df3809c4fe522
Reedman123 <114189712+Reedman123@users.noreply.github.com>
2025-10-30 20:44:05 -0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('clicker').onclick = function () 
-{
-  const element = document.getElementById('clicker');
-  element.style.color= "#fff";
-}
-
-const bgm = new Audio("steves-lava-chicken.mp3");
-bgm.loop = true;
-bgm.play();
